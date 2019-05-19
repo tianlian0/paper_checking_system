da98ac413ba57d68c20bcbb4bb6d05413b743c1e
mark_liu <liu13269455505@126.com>
2019-05-19 22:45:54 +0800

    1、日常引用库版本更新 2、bug修复 3、错别字修改；添加图片

--- paper_checking/MainForm.cs
+++ paper_checking/MainForm.cs
@@ -338,10 +338,11 @@ namespace paper_checking
             string[] param = new string[4] { textBox9.Text, paper_source, txt_paper_source, "0" };
             t1.Start(param);
             t1.Join();
-            
+
+            Thread t2 = new Thread(new ParameterizedThreadStart(start_convert_PDF));
             param = new string[4] { textBox9.Text, to_check_paper, to_check_txt_paper, "0" };
-            t1.Start(param);
-            t1.Join();
+            t2.Start(param);
+            t2.Join();
 
             for (int i = 0; i < int.Parse(textBox8.Text); i++)
             {
